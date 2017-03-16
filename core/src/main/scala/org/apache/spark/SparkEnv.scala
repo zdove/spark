31b825a0c5f3b84965f28a214f5902b1fa2a8105
zdove <596026098@qq.com>
2017-03-16 10:18:32 +0800

    Update SparkEnv.scala

--- core/src/main/scala/org/apache/spark/SparkEnv.scala
+++ core/src/main/scala/org/apache/spark/SparkEnv.scala
@@ -70,6 +70,7 @@ class SparkEnv (
     val conf: SparkConf) extends Logging {
 
   private[spark] var isStopped = false
+    var currentStage: Int = -1
   private val pythonWorkers = mutable.HashMap[(String, Map[String, String]), PythonWorkerFactory]()
 
   // A general, soft-reference map for metadata needed during HadoopRDD split computation
