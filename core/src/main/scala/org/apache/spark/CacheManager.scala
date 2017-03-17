23f35e4ef168d353529bd8c8a5c23b224f68a411
zdove <596026098@qq.com>
2017-03-17 14:11:39 +0800

    Update CacheManager.scala

--- core/src/main/scala/org/apache/spark/CacheManager.scala
+++ core/src/main/scala/org/apache/spark/CacheManager.scala
@@ -1,4 +1,4 @@
- def getOrCompute[T](
+def getOrCompute[T](
       rdd: RDD[T],
       partition: Partition,
       context: TaskContext,
@@ -21,6 +21,36 @@
           }
         }
       case None =>
+      //  val blockManager = SparkEnv.get.blockManager
+        if (!blockManager.blockExInfo.containsKey(key)) {
+          blockManager.blockExInfo.put(key, new BlockExInfo(key))
+        }
+        blockManager.stageExInfos.get(blockManager.currentStage) match {
+          case Some(curStageExInfo) =>
+            var parExist = true
+            if(curStageExInfo.depMap.contains(rdd.id) && curStageExInfo.depMap(rdd.id).size!= 0 ){
+            for (par <- curStageExInfo.depMap(rdd.id)) {
+              val parBlockId = new RDDBlockId(par, partition.index)
+              if (blockManager.blockExInfo.containsKey(parBlockId) &&
+                blockManager.blockExInfo.get(parBlockId).isExist
+                  == 1) { // par is exist
+
+              } else { // par not exist now, add this key to it's par's watching set
+                parExist = false
+                if (!blockManager.blockExInfo.containsKey(parBlockId)) {
+                  blockManager.blockExInfo.put(parBlockId, new BlockExInfo(parBlockId))
+                }
+                blockManager.blockExInfo.get(parBlockId).sonSet += key
+              }
+            }
+            if (parExist) { // par are all exist so we update this rdd's start time
+              logTrace("par all exist, store start time of " + key)
+              blockManager.blockExInfo.get(key).creatStartTime = System.currentTimeMillis()
+            }
+            }
+          case None =>
+            logError("Some Thing Wrong")
+        }
         // Acquire a lock for loading this partition
         // If another thread already holds the lock, wait for it to finish return its results
         val storedValues = acquireLockForPartition[T](key)
@@ -28,40 +58,8 @@
           return new InterruptibleIterator[T](context, storedValues.get)
         }
 
-      val blockkey = new RDDBlockId(rdd.id, partition.index)
-      logInfo(s"Partition $blockkey not found, computing it")
-
-      val blockManager = SparkEnv.get.blockManager
-
-      if (!blockManager.blockExInfo.containsKey(blockkey)) {
-        blockManager.blockExInfo.put(blockkey, new BlockExInfo(blockkey))
-      }
-
-      blockManager.stageExInfos.get(blockManager.currentStage) match {
-        case Some(curStageExInfo) =>
-          var parExist = true
-          for (par <- curStageExInfo.depMap(rdd.id)) {
-            val parBlockId = new RDDBlockId(par, partition.index)
-            if (blockManager.blockExInfo.containsKey(parBlockId) &&
-              blockManager.blockExInfo.get(parBlockId).isExist
-                == 1) { // par is exist
-
-            } else { // par not exist now, add this key to it's par's watching set
-              parExist = false
-              if (!blockManager.blockExInfo.containsKey(parBlockId)) {
-                blockManager.blockExInfo.put(parBlockId, new BlockExInfo(parBlockId))
-              }
-              blockManager.blockExInfo.get(parBlockId).sonSet += blockkey
-            }
-          }
-          if (parExist) { // par are all exist so we update this rdd's start time
-            logTrace("par all exist, store start time of " + blockkey)
-            blockManager.blockExInfo.get(blockkey).creatStartTime = System.currentTimeMillis()
-          }
-        case None =>
-          logError("Some Thing Wrong")
-      }
-
+      // val blockkey = new RDDBlockId(rdd.id, partition.index)
+      logInfo(s"Partition $key not found, computing it")
 
         // Otherwise, we have to load the partition ourselves
         try {
@@ -70,7 +68,7 @@
 
           // 此partition计算结束返回计算时间，得到createcost
 
-          blockManager.blockExInfo.get(blockkey).writeFinAndCalCreatCost(System.currentTimeMillis())
+          blockManager.blockExInfo.get(key).writeFinAndCalCreatCost(System.currentTimeMillis())
 
           // If the task is running locally, do not persist the result
           if (context.isRunningLocally) {
@@ -93,3 +91,4 @@
         }
     }
   }
+
